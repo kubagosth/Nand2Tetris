64c531e771090363d36a3a29c94dde3af4214909
Jakub Grabarski (JAKU0065) <jaku0065@zbc.dk>
2023-02-12 16:09:13 +0100

    Upload Project 6

---
+++ Other/AssemblerTranslator/DefinedValues.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblerTranslator
+{
+    internal class DefinedValues
+    {
+        public Dictionary<string, string> aDictionary = new();
+        public Dictionary<string, string> destDictionary = new()
+        {
+            {"null", "000" },
+            {"M",    "001"  },
+            {"D",    "010"  },
+            {"MD",   "011"  },
+            {"A",    "100"  },
+            {"AM",   "101"  },
+            {"AD",   "110"  },
+            {"AMD",  "111"  },
+
+        };
+        public Dictionary<string, string> jumpDictionary = new()
+        {
+            {"null", "000" },
+            {"JGT", "001" },
+            {"JEQ", "010" },
+            {"JGE", "011" },
+            {"JLT", "100" },
+            {"JNE", "101" },
+            {"JLE", "110" },
+            {"JMP", "111" },
+        };
+        public Dictionary<string, string> compDictionary = new()
+        {
+            {"0", "0101010" },
+            {"1", "0111111" },
+            {"-1", "0111010" },
+            {"D", "0001100" },
+            {"A", "0110000" },
+            {"M", "1110000" },
+            {"!D", "0001101" },
+            {"!A", "0110001" },
+            {"!M", "1110001" },
+            {"D+1", "0011111" },
+            {"A+1", "0110111" },
+            {"M+1", "1110111" },
+            {"D-1", "0001110" },
+            {"A-1", "0110010" },
+            {"M-1", "1110010" },
+            {"D+A", "0000010" },
+            {"D+M", "1000010" },
+            {"D-A", "0010011" },
+            {"D-M", "1010011" },
+            {"A-D", "0000111" },
+            {"M-D", "1000111" },
+            {"D&A", "0000000" },
+            {"D&M", "1000000" },
+            {"D|A", "0010101" },
+            {"D|M", "1010101" }
+        };
+
+        public DefinedValues()
+        {
+            aDictionary.Add("R0", ConvertToBitString(0));
+            aDictionary.Add("R1", ConvertToBitString(1));
+            aDictionary.Add("R2", ConvertToBitString(2));
+            aDictionary.Add("R3", ConvertToBitString(3));
+            aDictionary.Add("R4", ConvertToBitString(4));
+            aDictionary.Add("R5", ConvertToBitString(5));
+            aDictionary.Add("R6", ConvertToBitString(6));
+            aDictionary.Add("R7", ConvertToBitString(7));
+            aDictionary.Add("R8", ConvertToBitString(8));
+            aDictionary.Add("R9", ConvertToBitString(9));
+            aDictionary.Add("R10", ConvertToBitString(10));
+            aDictionary.Add("R11", ConvertToBitString(11));
+            aDictionary.Add("R12", ConvertToBitString(12));
+            aDictionary.Add("R13", ConvertToBitString(13));
+            aDictionary.Add("R14", ConvertToBitString(14));
+            aDictionary.Add("R15", ConvertToBitString(15));
+            aDictionary.Add("SCREEN", ConvertToBitString(16384));
+            aDictionary.Add("KBD", ConvertToBitString(24576));
+            aDictionary.Add("SP", ConvertToBitString(0));
+            aDictionary.Add("LCL", ConvertToBitString(1));
+            aDictionary.Add("ARG", ConvertToBitString(2));
+            aDictionary.Add("THIS", ConvertToBitString(3));
+            aDictionary.Add("THAT", ConvertToBitString(4));
+        }
+        public string ConvertToBitString(int value)
+        {
+            string s = Convert.ToString(value, 2); 
+
+            int[] bits = s.PadLeft(16, '0').Select(c => int.Parse(c.ToString())).ToArray();
+            return string.Join("", bits);
+        }
+    }
+}
---
+++ Other/AssemblerTranslator/Program.cs
@@ -0,0 +1,164 @@
+﻿using System.Text;
+
+namespace AssemblerTranslator
+{
+    internal class Program
+    {
+        static string path = "C:\\Users\\jaku0065\\Desktop\\nand2tetris\\nand2tetris\\projects\\06\\Pong\\pong.asm";
+        static DefinedValues definedValues = new DefinedValues();
+        static StringBuilder binaryString = new StringBuilder();
+        
+        static void Main(string[] args)
+        {
+            if (binaryString.Length > 0)
+            {
+                binaryString.Clear();
+            }
+
+            if (File.Exists(path))
+            {
+                string[] file = File.ReadAllLines(path);
+                RemoveComments(file);
+                AddLabels(file);
+                AddVariables(file);
+                ConvertFile(file);
+            }
+
+            File.WriteAllText(Environment.CurrentDirectory + "/output.hack", binaryString.ToString());
+        }
+
+        private static void RemoveComments(string[] file)
+        {
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (file[i].Contains("//"))
+                {
+                    int index = file[i].IndexOf("//");
+                    if (index >= 0)
+                    {
+                        file[i] = file[i].Substring(0, index);
+                    }
+                }
+            }
+        }
+        private static void AddLabels(string[] file)
+        {
+            int counter = 0;
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(file[i]))
+                {
+                    if (file[i].Contains('('))
+                    {
+                        string label = file[i].Split('(', ')')[1];
+                        if (!definedValues.aDictionary.ContainsKey(label))
+                        {
+                            definedValues.aDictionary.Add(label, definedValues.ConvertToBitString(counter));
+                            file[i] = "";
+                            continue;
+                        }
+                    }
+                    counter++;
+                }
+            }
+        }
+        private static void AddVariables(string[] file)
+        {
+            int counter = 16;
+            foreach (var line in file)
+            {
+                if (line.Contains('@'))
+                {
+                    if (!char.IsDigit(line[line.IndexOf('@') + 1]))
+                    {
+                        string variable = line.Split('@', ' ')[1];
+                        if (!definedValues.aDictionary.ContainsKey(variable))
+                        {
+                            definedValues.aDictionary.Add(variable, definedValues.ConvertToBitString(counter));
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+        private static void ConvertFile(string[] file)
+        {
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(file[i]))
+                {
+                    //A instruction
+                    if (file[i].Contains('@'))
+                    {
+                        string variableOrNum = file[i].Split('@', ' ')[1];
+                        if (definedValues.aDictionary.ContainsKey(variableOrNum))
+                        {
+                            binaryString.AppendLine(string.Join("", definedValues.aDictionary[variableOrNum]));
+                        }
+                        else
+                        {
+                            binaryString.AppendLine(string.Join("", definedValues.ConvertToBitString(Convert.ToInt32(variableOrNum))));
+                        }
+                    }
+                    //C instruction 
+                    else
+                    {
+                        string dest;
+                        string comp;
+                        string jump;
+                        // first 3 bits are always 111
+                        binaryString.Append(111);
+
+                        if (file[i].Contains('='))
+                        {
+                            dest = file[i].Split(' ', '=')[0];
+                        }
+                        else
+                        {
+                            dest = "null";
+                        }
+
+
+                        // if file contains ; then jump is not null
+                        // else it is null
+                        if (file[i].Contains(';'))
+                        {
+                            jump = file[i].Split(';', ' ')[1];
+                        }
+                        else
+                        {
+                            jump = "null";
+                        }
+                        // if dest is null and jump is not null then comp is the first word
+                        if (dest == "null" && jump != "null")
+                        {
+                            comp = file[i].Split(' ', ';')[0];
+                        }
+                        else if (dest != "null" && jump == "null")
+                        {
+                            comp = file[i].Split('=', ' ')[1];
+                        }
+                        else
+                        {
+                            comp = file[i].Split('=', ';')[1];
+                        }
+
+                        try
+                        {
+                            comp = string.Join("", definedValues.compDictionary[comp]);
+                            dest = string.Join("", definedValues.destDictionary[dest]);
+                            jump = string.Join("", definedValues.jumpDictionary[jump]);
+                            binaryString.Append(comp);
+                            binaryString.Append(dest);
+                            binaryString.AppendLine(jump);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
